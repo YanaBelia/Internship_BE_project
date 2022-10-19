94fb0d315966a89347d03457e616663fbaa9692e
YanaBelia <ivanna.belia138@gmail.com>
2022-10-19 20:31:43 +0300

    Alembic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 import databases
-import sqlalchemy
 import aioredis
 import os
 from dotenv import load_dotenv
 from app.config import POSTGRES_URL, REDIS_URL
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 basedir = os.path.abspath(os.path.dirname("../"))
@@ -12,6 +12,15 @@ load_dotenv(dotenv_path=f"{basedir}/.env")
 
 db = databases.Database(POSTGRES_URL)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,                  #Add CORSMiddleware from BE-3*
+    allow_headers=["*"],
+    allow_methods=["*"],
+)
 @app.get("/")
 async def root():
     return{"status": "Working"}
